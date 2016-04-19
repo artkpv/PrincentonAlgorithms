257ff5b982485cb76529cc74aa6a5ef4a7bc4c4e
w1ld <artyomkarpov@gmail.com>
2016-04-19 17:38:07 +0300

    Initial

---
+++ javac-algs4.bat
@@ -0,0 +1,13 @@
+@echo off
+
+:: ***************************************************
+:: javac-algs4
+:: ------------------
+:: Wrapper for javac that includes stdlib.jar 
+:: and algs4.jar
+:: ***************************************************
+
+set install=%USERPROFILE%\algs4
+
+javac -cp ".;%install%\stdlib.jar;%install%\algs4.jar" -g -encoding UTF-8 %*
+
