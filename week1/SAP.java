e1582493c3d6df9d9edc0040b3273878a71cc39d
w1ld <artyomkarpov@gmail.com>
2016-04-30 19:25:24 +0300

    SAP: if ancestor and length to the given vertex

--- week1/SAP.java
+++ week1/SAP.java
@@ -7,6 +7,8 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class SAP {
     private Digraph digraph;
+    private int minLength;
+    private int ancestor;
 
     // constructor takes a digraph (not necessarily a DAG)
     public SAP(Digraph G) {
@@ -15,11 +17,15 @@ public class SAP {
 
     // length of shortest ancestral path between v and w; -1 if no such path
     public int length(int v, int w) {
+        bfs(v, w);
+        return minLength;
+    }
+
+    private void bfs(int v, int w) {
         if (v < 0 || v >= digraph.V())
             throw new IndexOutOfBoundsException("vertex " + v + " is not between 0 and " + (digraph.V()-1));
         if (w < 0 || w >= digraph.V())
             throw new IndexOutOfBoundsException("vertex " + w + " is not between 0 and " + (digraph.V()-1));
-
         BreadthFirstDirectedPaths bfForV = new BreadthFirstDirectedPaths(digraph, v);
 
         Queue<Integer> q = new Queue<Integer>();
@@ -27,43 +33,11 @@ public class SAP {
         int[] distTo = new int[digraph.V()];
         q.enqueue(w);
         distTo[w] = 0;
-        int minLength = -1;
+        ancestor = w;
+        minLength = 0;
         while (!q.isEmpty()) {
             int r = q.dequeue();
 
-            if (bfForV.hasPathTo(r)) {
-                int length =  bfForV.distTo(r) + distTo[r];
-                if (length < minLength)
-                    minLength = length; 
-            }
-
-            for (int s : digraph.adj(r)) {
-                if (!marked[s]) {
-                    marked[s] = true;
-                    distTo[s] = distTo[r] + 1;
-                    q.enqueue(s);
-                }
-            }
-        }
-        return minLength;
-    }
-
-    // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
-    public int ancestor(int v, int w) {
-        if (v < 0 || v >= digraph.V())
-            throw new IndexOutOfBoundsException("vertex " + v + " is not between 0 and " + (digraph.V()-1));
-        if (w < 0 || w >= digraph.V())
-            throw new IndexOutOfBoundsException("vertex " + w + " is not between 0 and " + (digraph.V()-1));
-        BreadthFirstDirectedPaths bfForV = new BreadthFirstDirectedPaths(digraph, v);
-
-        Queue<Integer> q = new Queue<Integer>();
-        boolean[] marked = new boolean[digraph.V()];
-        int[] distTo = new int[digraph.V()];
-        q.enqueue(w);
-        int minLength = -1;
-        int ancestor = -1;
-        while (!q.isEmpty()) {
-            int r = q.dequeue();
             if (bfForV.hasPathTo(r)) {
                 int length =  bfForV.distTo(r) + distTo[r];
                 if (length < minLength) {
@@ -80,6 +54,11 @@ public class SAP {
                 }
             }
         }
+    }
+
+    // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
+    public int ancestor(int v, int w) {
+        bfs(v, w);
         return ancestor;
     }
 
