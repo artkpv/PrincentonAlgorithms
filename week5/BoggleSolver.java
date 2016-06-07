b61d6b9eadbd961135ed4e26098089f8fb97a472
w1ld <artyomkarpov@gmail.com>
2016-06-07 19:14:55 +0300

    Solved -> optimized (broken)

--- week5/BoggleSolver.java
+++ week5/BoggleSolver.java
@@ -8,7 +8,9 @@ import java.lang.String;
 import java.lang.StringBuilder;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Stack;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.TrieST;
 
@@ -35,188 +37,79 @@ public class BoggleSolver
         ArrayList<String> words = new ArrayList<String>();
         for(int i = 0; i < board.rows(); i++){
             for(int j = 0; j < board.cols(); j++){
-                words.addAll(getAllValidWords(board, i, j));
+                Stack<Point> path = new Stack<Point>();
+                Point p = new Point(i, j);
+                path.push(p);
+                StringBuilder wordBuilder = new StringBuilder();
+                append(wordBuilder, board, p);
+                walk(board, path, words, wordBuilder);
             }
         }
         trace("found " + words.size() + " words");
         return words;
     }
 
-    private ArrayList<String> getAllValidWords(BoggleBoard board, int row, int col) {
-        trace(" searching words at " + row + ", " + col);
-        ArrayList<String> found = new ArrayList<String>();
+    private void append(StringBuilder sb, BoggleBoard board, Point p) {
+        char l = board.getLetter(p.Row, p.Col);
+        sb.append(l);
+        if(l == 'Q')
+            sb.append('U');
+    }
 
-        String current;
-        BoardIterator iterator = new BoardIterator(board, row, col);
-        while((current = iterator.next()) != null) {
-            trace(" iterator.next() = '" + current + "'");
-            if(current.length() < 3) 
+    private void remove(StringBuilder sb, BoggleBoard board, Point p) {
+        char l = board.getLetter(p.Row, p.Col);
+        sb.deleteCharAt(sb.length() - 1); 
+        if(l == 'Q')
+            sb.deleteCharAt(sb.length() - 1); // 'U' was removed
+    }
+
+
+    private void walk(BoggleBoard board, Stack<Point> path, ArrayList<String> found, StringBuilder wordBuilder) {
+        Point v = path.peek();
+        for(Point p : getAdj(board, v)) {
+            if(path.contains(p))
                 continue;
 
-            Iterator<String> keys = dictionaryTrie.keysWithPrefix(current).iterator();
+            path.push(p);
+            append(wordBuilder, board, p);
+            String current = wordBuilder.toString();
 
-            String key;
-            // nothing on that path:
-            if(keys == null || !keys.hasNext() || (key = keys.next()) == null) 
-            {
-                // returned to start:
-                if(current.length() == 3)
-                    break;
-                else
-                    iterator.skipPath();
+            if(current.length() < 3) {
+                walk(board, path, found, wordBuilder);
             }
-            // only one left is current:
-            else if(key == current) 
-                found.add(key);
-            // else many left
-
-        }
-
-        return found;
-    }
-
-    // TODO: make it depth first search fully (w/o previous node)
-    // depth first search on board
-    private class BoardIterator {
-
-        private int[] edgeTo; // previous edge
-        private BoggleBoard board;
-        private int current, toSkip;
-
-        public BoardIterator(BoggleBoard myBoard, int row, int col) {
-            board = myBoard;
-
-            edgeTo = new int[board.rows() * board.cols()];
-            for(int i = 0; i < edgeTo.length; i++) {
-                edgeTo[i] = -1;
-            }
-
-            current = getNumber(row, col);
-            edgeTo[current] = current;
-            toSkip = -1;
-        }
-
-        // bread first search:
-        public String next() {
-            trace("  BoardIterator|next() current=" + current + ", toSkip=" + toSkip);
-            int next = nextInternal(current, toSkip);
-
-            // run out for current:
-            if(next == -1) {
-                skipPath();
-                // can not skip hence run out all:
-                if(toSkip == -1) 
-                    return null;
-                // get next skipping current:
-                return next();
-            }
-
-            toSkip = -1;
-            edgeTo[next] = current;
-            current = next;
-            return getString(current);
-        }
-
-        public void skipPath() {
-            trace("  BoardIterator|skipPath() current=" + current);
-            int v = edgeTo[current];
-            edgeTo[current] = -1;
-            if(v == -1) // run out
-                return;
-
-            toSkip = current;
-            current = v;
-        }
-
-        private int nextInternal(int v, int previous) {
-            trace("  BoardIterator|nextInternal(" + v + ", previous=" + previous + ")");
-            if(v == -1) return -1; // got to the beginning
-
-            // convert indexes to rows, cols
-            int[] rowCol = getRowCol(v);
-            int row = rowCol[0], col = rowCol[1];
-            int pRow = -1, pCol = -1;
-            if(previous >= 0 ) {
-                int[] rowColPrevious = getRowCol(previous);
-                pRow = rowColPrevious[0]; pCol = rowColPrevious[1];
-            }
-
-            int rows = board.rows(), cols = board.cols();
-
-            final int N = 3;
-            for(int i = row - N/2; i <= row + N/2; i++) {
-                for(int j = col - N/2; j <= col + N/2; j++) {
-                    //trace("  BoardIterator|getNextAdj| i=" + i + ", j=" + j);
-                    // out of board:
-                    if(i < 0 || j < 0 || i >= rows || j >= cols) continue;
-                    // center: 
-                    if(i == row && j == col) continue;
-                    // before previous:
-                    if(i <= pRow && j <= pCol) continue;
-
-                    int next = getNumber(i, j);
-                    // attended before:
-                    if(edgeTo[next] != -1)  continue;
-                    else                    return next;
+            else {
+                Iterable<String> keys = dictionaryTrie.keysWithPrefix(current);
+                int left = 0;
+                for(String k : keys) {
+                    if(k == current)
+                        if(!found.contains(current))
+                            found.add(current);
+                    else
+                        left++;
                 }
+                if(left > 0)
+                    walk(board, path, found, wordBuilder);
             }
-            return -1;
-        }
 
-        private String getString(int v) {
-            trace(" BoggleIterator|getString(" + v + ")");
-            trace(" BoggleIterator|getString|edgeTo:" );
-            trace(get_string_of_edgeto());
-
-            if(v == -1) return null;
-            make_assert(0 <= v && v < edgeTo.length);
-
-            StringBuilder b = new StringBuilder();
-            while(v != -1){
-                int[] rowCol = getRowCol(v);
-                b.insert(0, board.getLetter(rowCol[0], rowCol[1]));
-                int next = edgeTo[v];
-                if(next == v)
-                    break;
-                v = next;
-            }
-            String str = b.toString();
-            make_assert(str != null);
-            return str;
-        }
-
-        private int getNumber(int row, int col) {
-            make_assert(
-                    (0 <= row && row < board.rows()) || 
-                    (0 <= col && col < board.cols()));
-            int number = board.cols() * row + col;
-            make_assert(0 <= number && number < edgeTo.length);
-            return number;
-        }
-
-        private int[] getRowCol(int number) {
-            make_assert(0 <= number && number < edgeTo.length);
-            int[] rowCol = 
-                new int[] { (number / board.cols() ), number % board.cols() }; 
-            make_assert(
-                    (0 <= rowCol[0] && rowCol[0] < board.rows()) || 
-                    (0 <= rowCol[1] && rowCol[1] < board.cols()));
-            return rowCol;
-        }
-
-        private String get_string_of_edgeto() {
-            StringBuilder sb = new StringBuilder();
-
-            for(int i = 0; i < edgeTo.length; i++) {
-                if(i != 0 && i % board.cols() == 0)
-                    sb.append("\n");
-                int num = edgeTo[i];
-                sb.append((num == -1 ? "?" : num) + " ");
-            }
-            return sb.toString();
+            path.pop();
+            remove(wordBuilder, board, p);
         }
     }
 
+    private Iterable<Point> getAdj(BoggleBoard board, Point p) {
+        ArrayList<Point> adj = new ArrayList<Point>();
+        for(int i = p.Row - 1; i <= p.Row + 1; i++) {
+            for(int j = p.Col - 1; j <= p.Col + 1; j++) {
+                boolean atBoard = (0 <= i && i < board.rows()) && (0 <= j && j < board.cols());
+                boolean atCenter = i == p.Row && j == p.Col;
+                if(!atBoard || atCenter)
+                    continue;
+                adj.add(new Point(i, j));
+            }
+        }
+        return adj;
+    }
+ 
     // Returns the score of the given word if it is in the dictionary, zero otherwise.
     // (You can assume the word contains only the uppercase letters A through Z.)
     public int scoreOf(String w) {
@@ -248,16 +141,29 @@ public class BoggleSolver
         StdOut.println("Score = " + score);
     }
 
+    private class Point {
+        public int Row, Col;
+
+        public Point(int row, int col) {
+            Row = row;
+            Col = col;
+        }
+
+        public boolean equals(Object other) {
+            if (other == this) return true;
+            if (other == null) return false;
+            if (getClass() != other.getClass()) return false;
+            Point point = (Point)other;
+            return (Row == point.Row && Col == point.Col);
+        }
+
+        public int hashCode() {
+            int hash = Row;
+            return hash * 17  + Col;
+        }
+    }
+
     private void trace(String msg) {
-        StdOut.println(msg);
-    }
-
-    private static void make_assert(boolean check) {
-        make_assert(check, null);
-    }
-
-    private static void make_assert(boolean check, String msg) {
-        if(!check)
-            throw new AssertionError("Assert failed. " + msg);
+        //StdOut.println(msg);
     }
 }
